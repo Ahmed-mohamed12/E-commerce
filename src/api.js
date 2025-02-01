ad5ac62d6bcdcfbdb015e17ef3fe7ac5eabd93ab
Ahmed <am9718366@gmail.com>
2025-02-01 22:28:27 +0200

    first

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-export const basic="https://backend-e-commerce-production-25ff.up.railway.app/api"
+export const basic="https://backend-e-commerce-production-181e.up.railway.app/api"
 export const registerUrl="register"
 export const LoginUrl="login"
 export const LogOutUrl="logout"
